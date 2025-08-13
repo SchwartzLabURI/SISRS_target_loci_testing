8100d2fe60c84782df3562465e5518b8643b51f7
rachelss <rachelss@users.noreply.github.com>
2025-08-13 16:02:59 -0400

    make fig 3 two panels

--- fig3.R
+++ fig3.R
@@ -16,15 +16,43 @@ rooted_tree <- drop.tip(rooted_tree, "Centro_rex_A367_Lago806")
 
 t <- ggtree(rooted_tree, layout="rectangular", size=1) + 
   hexpand(.7) + #how much space on left and right
-  annotate("point", x=0, y=13, shape=21, fill="darkgray", color="black", size=3) + 
-  annotate("text", x=0.2, y=13, label = "> 75% node support", hjust = "left", size=2) + 
-  geom_nodepoint(aes(subset = !is.na(as.numeric(label)) & as.numeric(label) > 0.75), size=3, 
-                 shape=21, fill="darkgray", color="black")
-t2 <- t %<+% data2 + geom_tippoint(shape=19, size=2.5) + 
+  geom_tree() +
+  geom_text2(
+    aes(label = label, subset = !isTip ),
+    vjust = -0.5,
+    hjust = 1.3,
+    size = 2.5
+  ) +
+  theme_tree()
+
+t2 <- t %<+% data2 + 
   theme(legend.position = "none") + 
   geom_tiplab(aes(label=NewLab), align=FALSE, hjust=-.02, parse=T, 
               family="Helvetica", size = 2) 
 ggsave(plot = t2, filename = "SISRS_marker_Bur.tre.png", 
        width = 3, height = 4.5, units = "in", limitsize = FALSE)
 
+tree <- read.tree("BurSCG_SpeciesTree_supercontigs_astral3.tre")
+rooted_tree <- root(tree, outgroup="Bur_isabellinus_A190_Lago405", resolve.root = TRUE, edgelabel = TRUE)
+rooted_tree$edge.length[which(is.na(rooted_tree$edge.length))] <- 0
 
+t3 <- ggtree(rooted_tree, layout="rectangular", size=1) + 
+  hexpand(.7) + #how much space on left and right
+  geom_tree() +
+  geom_text2(
+    aes(label = label, subset = !isTip ),
+    vjust = -0.5,
+    hjust = 1.3,
+    size = 2.5
+  ) +
+  theme_tree()
+t4 <- t3 %<+% data2 +
+  theme(legend.position = "none") + 
+  geom_tiplab(aes(label=NewLab), align=FALSE, hjust=-.02, parse=T, 
+              family="Helvetica", size = 2) 
+ggsave(plot = t2, filename = "BurSCG_SpeciesTree_supercontigs_astral3.tre.png", 
+       width = 3, height = 4.5, units = "in", limitsize = FALSE)
+
+combined_plot <- grid.arrange(t2, t4, ncol = 2)
+ggsave("Burm_both_locus_trees.png", plot = combined_plot, 
+       width = 6, height = 4.5, units = "in")
