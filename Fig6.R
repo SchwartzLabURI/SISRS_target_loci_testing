3fbc2fe251ee2d700a8cd910a62b9a01e11ddb79
cbreusing <77281303+cbreusing@users.noreply.github.com>
2025-08-15 08:40:52 -0400

    Update Fig6.R

--- Fig6.R
+++ Fig6.R
@@ -33,7 +33,14 @@ t2 <- t %<+% data2 +
   theme(legend.position = "none") + 
   geom_tiplab(aes(label=NewLab), align=FALSE, hjust=-.02, parse=T, family="Helvetica", size=3) + 
   aes(color=factor(Genus)) + 
-  scale_color_manual(values = col, name="Genus", na.value="black")
+  scale_color_manual(values = col, name="Genus", na.value="black") + 
+  geom_cladelab(node=97, label="Brevilimbatids", family="Helvetica", fontface="plain", offset=1.306) +
+  geom_cladelab(node=93, label="Burmeisterids", family="Helvetica", fontface="plain", offset=0.9) +
+  geom_cladelab(node=111, label="giganteus grade", family="Helvetica", fontface="plain", offset=1.58) +
+  geom_cladelab(node=114, label="Peruvianids", family="Helvetica", fontface="plain", offset=1.418) +
+  geom_cladelab(node=149, label="Eucentropogonids", family="Helvetica", fontface="plain", offset=1.19) +
+  geom_cladelab(node=157, label="andinus clade", family="Helvetica", fontface="plain", offset=1.155) +
+  geom_cladelab(node=131, label="Colombianids", family="Helvetica", fontface="plain", offset=1.025)
 t2
 #dev.off()
 
