f4d46dbcbd2b79e03df452e1d2576f550e2d2e24
cbreusing <77281303+cbreusing@users.noreply.github.com>
2025-08-15 10:53:35 -0400

    Update Fig6.R

--- Fig6.R
+++ Fig6.R
@@ -24,9 +24,9 @@ rooted_tree$edge.length[which(is.na(rooted_tree$edge.length))] <- 0
 rooted_tree$tip.label[ !(rooted_tree$tip.label %in% data2$Label) ] #check for species not in metadata
 
 #pdf("SCG_SpeciesTree_supercontigs_astral3.pdf", width=15, height=15)
-t <- ggtree(rooted_tree, layout="rectangular", size=1) + geom_treescale(x=0, y=78) + xlim(0, 3.3) + 
-  annotate("point", x=0, y=75, shape=21, fill="darkgray", color="black", size=3) + 
-  annotate("text", x=0.05, y=75, label = "> 75% node support", hjust = "left") + 
+t <- ggtree(rooted_tree, layout="rectangular", size=1) + xlim(0, 3.5) + 
+  annotate("point", x=0, y=80, shape=21, fill="darkgray", color="black", size=3) + 
+  annotate("text", x=0.05, y=80, label = "> 75% node support", hjust = "left") + 
   geom_nodepoint(aes(subset = !is.na(as.numeric(label)) & as.numeric(label) > 0.75), size=3, shape=21, fill="darkgray", color="black")
 t2 <- t %<+% data2 + 
   #geom_tippoint(aes(color=factor(Genus)), shape=19, size=1) + 
@@ -34,13 +34,13 @@ t2 <- t %<+% data2 +
   geom_tiplab(aes(label=NewLab), align=FALSE, hjust=-.02, parse=T, family="Helvetica", size=3) + 
   aes(color=factor(Genus)) + 
   scale_color_manual(values = col, name="Genus", na.value="black") + 
-  geom_cladelab(node=97, label="Brevilimbatids", family="Helvetica", fontface="plain", offset=1.006) +
-  geom_cladelab(node=93, label="Burmeisterids", family="Helvetica", fontface="plain", offset=0.6) +
-  geom_cladelab(node=111, label="giganteus grade", family="Helvetica", fontface="plain", offset=1.28) +
-  geom_cladelab(node=114, label="Peruvianids", family="Helvetica", fontface="plain", offset=1.118) +
-  geom_cladelab(node=149, label="Eucentropogonids", family="Helvetica", fontface="plain", offset=0.89) +
-  geom_cladelab(node=157, label="andinus clade", family="Helvetica", fontface="plain", offset=0.855) +
-  geom_cladelab(node=131, label="Colombianids", family="Helvetica", fontface="plain", offset=0.725)
+  geom_cladelab(node=97, label="Brevilimbatids", family="Helvetica", fontface="plain", offset=1.306) +
+  geom_cladelab(node=93, label="Burmeisterids", family="Helvetica", fontface="plain", offset=0.9) +
+  geom_cladelab(node=111, label="giganteus grade", family="Helvetica", fontface="plain", offset=1.58) +
+  geom_cladelab(node=114, label="Peruvianids", family="Helvetica", fontface="plain", offset=1.418) +
+  geom_cladelab(node=149, label="Eucentropogonids", family="Helvetica", fontface="plain", offset=1.19) +
+  geom_cladelab(node=157, label="andinus clade", family="Helvetica", fontface="plain", offset=1.155) +
+  geom_cladelab(node=131, label="Colombianids", family="Helvetica", fontface="plain", offset=1.025)
 t2
 #dev.off()
 
