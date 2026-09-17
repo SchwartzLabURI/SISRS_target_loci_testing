7047a99b7012b352527e6f3b2407a68cba39fcd4
cbreusing <77281303+cbreusing@users.noreply.github.com>
2026-09-17 10:55:05 -0400

    Update fig4.R

--- R_scripts/fig4.R
+++ R_scripts/fig4.R
@@ -18,7 +18,7 @@ rooted_tree$edge.length[which(is.na(rooted_tree$edge.length))] <- 0
 
 ggtree(rooted_tree) + geom_text(aes(label=node), hjust=-0.3, size=2) + geom_tiplab(align=FALSE, size=2)
 
-pdf("SISRS_SpeciesTree_astral3.pdf", width=9, height=11)
+pdf("SISRS_SpeciesTree_TargetCapture_astral3.pdf", width=9, height=11)
 t <- ggtree(rooted_tree, layout="rectangular", size=1) + 
 geom_treescale(x=0, y=120) + xlim(0, 12) + 
 annotate("point", x=0, y=117, shape=21, fill="darkgray", color="black", size=2) + 
